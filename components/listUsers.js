9d354b0b9c447a61d0abacdf041b65f2cdab9ba1
braulio0 <braulioelias6@gmail.com>
2021-01-24 20:39:45 -0600

    added router to admin page

--- components/listUsers.js
+++ components/listUsers.js
@@ -1,9 +1,10 @@
 import UserItem from "./UserItem";
+import hola from "../constant/scrollToID";
 
 const ListUser = ({ data }) => {
     return (
         <>
-            <table className="table">
+            <table className="table" id="home" >
                 <thead>
                     <tr>
                         <th scope="col">Nombre</th>
@@ -12,32 +13,30 @@ const ListUser = ({ data }) => {
                         <th scope="col">Zona de pesca</th>
                         <th scope="col"> Club de pesca </th>
                         <th scope="col">Estatus</th>
-                        <th scope ="col"> Responder </th>
+                        <th scope="col"> Responder </th>
                         <th scope="col"> Aceptar </th>
                     </tr>
                 </thead>
                 <tbody>
-                  
-                        {data.map(
-                            ({
-                                Username,
-                                email,
-                                licenseFishing,
-                                fishing,
-                                status,
-                                clubName,
-                            }) => (
-                                <UserItem
-                                    Username={Username}
-                                    email={email}
-                                    licenseFishing={licenseFishing}
-                                    fishing={fishing}
-                                    status={status}
-                                    clubName={clubName}
-                                />
-                            )
-                        )}
-                
+                    {data.map(
+                        ({
+                            Username,
+                            email,
+                            licenseFishing,
+                            fishing,
+                            status,
+                            clubName,
+                        }) => (
+                            <UserItem
+                                Username={Username}
+                                email={email}
+                                licenseFishing={licenseFishing}
+                                fishing={fishing}
+                                status={status}
+                                clubName={clubName}
+                            />
+                        )
+                    )}
                 </tbody>
             </table>
         </>
--- pages/Administrador.js
+++ pages/Administrador.js
@@ -2,13 +2,20 @@ import { useState, useEffect } from "react";
 import { withRouter } from "next/router";
 import axios from "axios";
 import ListUser from "./../components/listUsers";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+const logo = "logo_small.png";
 
 const Administrador = () => {
   const [user, setUser] = useState([]);
-  const apiGet = () => {
-    axios.get(`http://localhost:8080/solicitante/`).then(
+  const [data, setData] = useState(["PENDING"]);
+  const apiGet = (data) => {
+    axios.post(`http://localhost:8080/solicitante/${data}`).then(
       (response) => {
-        console.log(response.data);
+        //    console.log(response.data);
+
         setUser(response.data);
       },
       (error) => {
@@ -16,14 +23,76 @@ const Administrador = () => {
       }
     );
   };
+  const Aceptados = () => {
+    console.log("Rechazados");
+    setData("ACCEPTED");
+  };
+  const Pendiente = () => {
+    console.log("Pendiente");
+    setData("PENDING");
+  };
+  const Rechazados = () => {
+    console.log("Rechazados");
+    setData("REJECTED");
+  };
 
   useEffect(() => {
-    apiGet();
+    apiGet(data);
     console.log(user);
   }, []);
+  useEffect(() => {
+    console.log(data);
+    apiGet(data);
+  }, [data]);
 
-  return (<>
-          <ListUser data={ user }/>
-    </>);
+  return (
+    <>
+      <Navbar
+        className="fixed-top shadow-lg"
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+      >
+        <Navbar.Brand className="ml-lg-5">
+          <a class="nav-link" href="/">
+            <img style={{ width: "250px" }} src={logo} />
+          </a>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="justify-content-between w-100 mr-5">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 align-items-lg-center">
+              <li class="nav-item">
+                <a class="nav-link" href="#home">
+                  Inicio
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#clubs" onClick={Pendiente}>
+                  Pendientes{" "}
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#about" onClick={Aceptados}>
+                  Aceptados{" "}
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#about" onClick={Rechazados}>
+                  Rechazados
+                </a>
+              </li>
+            </ul>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <div className="container m-5 p-2">
+        <ListUser data={user} />
+      </div>
+    </>
+  );
 };
 export default withRouter(Administrador);
